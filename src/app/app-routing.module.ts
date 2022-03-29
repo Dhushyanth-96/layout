7ae8b02093569ddecab32270e8a25be17f288fae
Dhushyanth-96 <88694692+Dhushyanth-96@users.noreply.github.com>
2022-03-29 19:20:43 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SimpleComponent } from './layout/simple-layout/simple-component/simple-component.component';
+import { DashboardLayoutComponent } from './layout/dashboard-layout/dashboard-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', redirectTo:'auth/signin', pathMatch: 'full'
+  },
+  {
+    path:'',
+    component:SimpleComponent,
+    children: [
+      {
+        path:'auth',
+        loadChildren: () => import('./component/auth/auth.module').then(m => m.AuthModule)
+      }
+    ]
+  },
+  {
+    path:'',
+    component:DashboardLayoutComponent,
+    children: [
+      {
+        path:'dashboard',
+        loadChildren: () => import('./component/dashboard/dashboard.module').then(m => m.DashboardModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
