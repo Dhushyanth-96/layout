38364a5d9618e63e49219f7bcfe9cb35b6df4779
Dhushyanth-96 <88694692+Dhushyanth-96@users.noreply.github.com>
2022-03-29 19:20:58 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SimpleComponent } from './layout/simple-layout/simple-component/simple-component.component';
+import { DashboardLayoutComponent } from './layout/dashboard-layout/dashboard-layout.component';
+import { DashboardHeaderComponent } from './layout/dashboard-layout/dashboard-header/dashboard-header/dashboard-header.component';
+import { DashboardSidebarComponent } from './layout/dashboard-layout/dashboard-sidebar/dashboard-sidebar/dashboard-sidebar.component';
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardLayoutComponent,
+    DashboardHeaderComponent,
+    DashboardSidebarComponent,
+    SimpleComponent,
+    
   ],
   imports: [
     BrowserModule,
